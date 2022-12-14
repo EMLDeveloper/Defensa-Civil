511072c1b4d7fc52289824591e21e7c97ee16287
EMLdeveloper <EnmanuelleML@outlook.com>
2022-12-13 17:51:43 -0800

    apk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { RegistroPipe } from './registro.pipe';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import {AngularFireModule} from '@angular/fire/compat'
-import { environment } from 'src/environments/environment';
-import {AngularFireAuthModule} from '@angular/fire/compat/auth'
 
 @NgModule({
   declarations: [AppComponent, RegistroPipe],
-  imports: [AngularFireAuthModule,AngularFireModule.initializeApp(environment.firebaseConfig),BrowserModule, IonicModule.forRoot(), AppRoutingModule , HttpClientModule, ReactiveFormsModule,FormsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule , HttpClientModule, ReactiveFormsModule,FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
